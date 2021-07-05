6ed8942b3db95fa2f21a1b21ae7c2191266b514c
Подоскин Константин <68177747+siw321@users.noreply.github.com>
2021-07-05 20:59:42 +0700

    WriteToJournal and  GetJournal

---
+++ lib/src/storage.cpp
@@ -0,0 +1,28 @@
+#include "storage.h"
+
+class StorageInMemory : public IStorage {
+    bool WriteToJournal(std::vector<std::string> ops) override {
+        for (auto it : ops) {
+            journal.push_back(it);
+        }
+        return true;
+    }
+    bool AddTable(std::string blob) override {
+        return false;
+	}
+    ITableListPtr GetTableList() override {
+        return nullptr;
+    }
+    JournalBlob GetJournal() override {
+        return journal;
+    }
+private:
+    std::vector<std::string> journal;
+};
+
+IStoragePtr CreateStorage(DbSettings settings) {
+    if (settings.in_memory) {
+        IStoragePtr storage = std::make_shared<StorageInMemory>();
+        return storage;
+    }
+}
---
+++ test/storage_test.cpp
@@ -0,0 +1,12 @@
+#include "storage.h"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Storage", "[write get]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    auto storage = CreateStorage(settings);
+    storage->WriteToJournal({ "key,value,update" });
+    
+    REQUIRE(storage->GetJournal()[0] == "key,value,update");
+}
