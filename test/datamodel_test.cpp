630127bb8cd6ac4c4ea7c380a1a83908f2d9c622
abrg <abramov.g.evg@gmail.com>
2021-07-05 22:47:21 +0700

    journal implementation

--- test/datamodel_test.cpp
+++ test/datamodel_test.cpp
@@ -3,15 +3,30 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-TEST_CASE( "Datamodel", "[set get]" ) {
+TEST_CASE("Datamodel", "[set get]") {
     auto datamodel = CreateDatamodel(nullptr, DbSettings{});
 
     Operations ops;
-    ops.push_back(Op{"key", "value", Op::Type::Update});
+    ops.push_back(Op{ "key", "value", Op::Type::Update });
 
-    datamodel->Commit(ops);
+    datamodel->LSTMCommit(ops);
     std::string value;
-    datamodel->GetValue("key", value);
+    datamodel->LSMTGetValue("key", value);
 
-    REQUIRE( value == "value" );
+    REQUIRE(value == "value");
+}
+
+TEST_CASE("Datamodel", "[write get]") {
+    auto datamodel = CreateDatamodel(nullptr, DbSettings{});
+
+    Operations ops;
+    ops.push_back(Op{ "key", "value", Op::Type::Update });
+
+    DbSettings settings;
+    settings.in_memory = true;
+    auto storage = CreateStorage(settings);
+
+    datamodel->SerializeOps(ops, storage)
+
+    REQUIRE(storage->GetJournal[0] == "Update key value");
 }
