73f3cbc0529648ecc4e88391381c319ab78fa6ba
abrg <abramov.g.evg@gmail.com>
2021-07-08 23:36:38 +0700

    fixed testcases

--- test/datamodel_test.cpp
+++ test/datamodel_test.cpp
@@ -3,7 +3,24 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-TEST_CASE("Datamodel", "[set get]") {
+TEST_CASE("Datamodel1", "[set get]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    auto storage = CreateStorage(settings);
+    auto datamodel = CreateDatamodel(storage, DbSettings{});
+
+    Operations ops;
+    ops.push_back(Op{"key", "value", Op::Type::Update});
+
+    std::string value;
+    datamodel->Commit(ops);
+    datamodel->GetValue("key", value);
+
+    REQUIRE(value == "value");
+    REQUIRE(storage->GetJournal()[0] == "Update key value");
+}
+
+TEST_CASE("Datamodel2", "[set + remove get]") {
     DbSettings settings;
     settings.in_memory = true;
     auto storage = CreateStorage(settings);
@@ -12,16 +29,38 @@ TEST_CASE("Datamodel", "[set get]") {
     Operations ops;
     ops.push_back(Op{"key", "value", Op::Type::Update});
     ops.push_back(Op{"key", "", Op::Type::Remove});
-    ops.push_back(Op{"key space test", "value with spaces", Op::Type::Update});
+    ops.push_back(Op{"key", "value1", Op::Type::Update});
 
     std::string value;
-    datamodel->GetValue("key0", value);
-    REQUIRE(datamodel->GetValue("key0", value) == false);
-
     datamodel->Commit(ops);
-    datamodel->GetValue("key space test", value);
-    REQUIRE(value == "value with spaces");
+    datamodel->GetValue("key", value);
 
-    REQUIRE(storage->GetJournal()[0] == "Update key value");
-    REQUIRE(storage->GetJournal()[1] == "Remove key");
+    REQUIRE(value == "value1");
+}
+
+TEST_CASE("Datamodel3", "[set get w/empty key]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    auto storage = CreateStorage(settings);
+    auto datamodel = CreateDatamodel(storage, DbSettings{});
+
+    std::string value;
+
+    REQUIRE(datamodel->GetValue("key0", value) == false);
+}
+
+TEST_CASE("Datamodel4", "[set get w/spaces]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    auto storage = CreateStorage(settings);
+    auto datamodel = CreateDatamodel(storage, DbSettings{});
+
+    Operations ops;
+    ops.push_back(Op{"key with spaces", "value with spaces", Op::Type::Update});
+
+    std::string value;
+    datamodel->Commit(ops);
+    datamodel->GetValue("key with spaces", value);
+
+    REQUIRE(value == "value with spaces");
 }
