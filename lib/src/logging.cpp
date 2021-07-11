238667ddb43e53a5349082987d4989bc4d9a9db7
ruca905 <yarkivaev@mail.ru>
2021-07-12 05:05:23 +0700

    Add '\n' in ens of Info, Defug, Error outputs

--- lib/src/logging.cpp
+++ lib/src/logging.cpp
@@ -8,18 +8,18 @@ void SetLogLevel(LogLevel level) { globalLevel = level; }
 
 void Info(std::string input) {
     if (globalLevel == LogLevel::Info) {
-        std::cout << input;
+        std::cout << input + '\n';
     }
 }
 
 void Error(std::string input) {
     if (globalLevel == LogLevel::Error) {
-        std::cout << input;
+        std::cout << input + '\n';
     }
 }
 
 void Debug(std::string input) {
     if (globalLevel == LogLevel::Debug) {
-        std::cout << input;
+        std::cout << input + '\n';
     }
 }
