5053d5ca6a5d52747ae92359b9d1faf88d450b31
siw321 <podoskin.kostya@gmail.com>
2021-07-12 17:48:30 +0700

    journal recovery fixes

--- lib/src/storage.cpp
+++ lib/src/storage.cpp
@@ -59,8 +59,9 @@ public:
             return false;
         }
         for (auto const it : ops) {
-            int32_t tmp = std::hash<std::string>{}(it) % INT32_MAX;
-            file.write((const char*)&tmp, sizeof(int32_t));
+            uint32_t tmp = std::hash<std::string>{}(it) %
+                           std::numeric_limits<std::uint32_t>::max();
+            file.write((const char*)&tmp, sizeof(uint32_t));
             tmp = (it.size());
             file.write((const char*)&tmp, sizeof(int32_t));
             file << it;
@@ -105,21 +106,25 @@ public:
         if (!file) {
             return {};
         }
+        bool is_journal_damaged = false;
         while (!(feof(file))) {
-            int32_t hash;
-            fread((void*)&hash, sizeof(int32_t), 1, file);
+            uint32_t hash;
+            fread((void*)&hash, sizeof(uint32_t), 1, file);
             int32_t len;
             fread((void*)&len, sizeof(int32_t), 1, file);
             if (feof(file)) break;
             journal.push_back({});
             journal.back().resize(len);
             fread(journal.back().data(), sizeof(char), len, file);
-            assert(std::hash<std::string>{}(journal.back()) % INT32_MAX ==
-                   hash);
-            if (feof(file)) {
+            if (std::hash<std::string>{}(journal.back()) % INT32_MAX == hash) {
+                if (is_journal_damaged) throw "The journal is damaged";
+            } else if (feof(file)) {
                 journal.pop_back();
                 break;
-            }
+            } else {
+                journal.pop_back();
+                is_journal_damaged = true;
+            } 
         }
         fclose(file);
         return journal;
