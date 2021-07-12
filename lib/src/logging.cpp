3f396510f4acecc1160ef255a93178dd55204c1e
Yaroslav Kivaev <yarkivaev@mail.ru>
2021-07-12 09:32:53 +0700

    Add logging in project (#45)
    * Add logging in project
    
    * fix codestyle
    
    * Add '\n' in ens of Info, Defug, Error outputs
    
    * fix loglevel priority

---
+++ lib/src/logging.cpp
@@ -0,0 +1,26 @@
+#include "logging.h"
+
+#include <iostream>
+
+static LogLevel globalLevel = LogLevel::Info;
+
+void SetLogLevel(LogLevel level) { globalLevel = level; }
+
+void Info(std::string input) {
+    if (globalLevel == LogLevel::Info || globalLevel == LogLevel::Debug) {
+        std::cout << input + '\n';
+    }
+}
+
+void Error(std::string input) {
+    if (globalLevel == LogLevel::Error || globalLevel == LogLevel::Info ||
+        globalLevel == LogLevel::Debug) {
+        std::cout << input + '\n';
+    }
+}
+
+void Debug(std::string input) {
+    if (globalLevel == LogLevel::Debug) {
+        std::cout << input + '\n';
+    }
+}
---
+++ lib/src/logging.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+enum class LogLevel { Error, Info, Debug };
+
+void SetLogLevel(LogLevel level);
+
+void Info(std::string input);
+void Debug(std::string input);
+void Error(std::string input);
