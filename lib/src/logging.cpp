9f828be502cbed119994ba500f863150ab199ff5
Yaroslav Kivaev <yarkivaev@mail.ru>
2021-07-12 21:53:48 +0700

    Multithreaded logging (stdout only) (#46)
    * Add multithreading in logging
    
    * Add delay and fix CMakeList
    
    * fix codestyle
    
    * fix codestyle

--- lib/src/logging.cpp
+++ lib/src/logging.cpp
@@ -1,26 +1,76 @@
 #include "logging.h"
 
+#include <chrono>
 #include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 static LogLevel globalLevel = LogLevel::Info;
+static std::vector<std::string> logsQueue;
+static bool isLogThread = false;
+static bool stopThread = false;
+static std::thread logThread;
+std::mutex mutex;
 
 void SetLogLevel(LogLevel level) { globalLevel = level; }
 
+void StopLogThread() { stopThread = 1; }
+
+void LogThreadCycle() {
+    while (!stopThread) {
+        if (!logsQueue.empty()) {
+            std::vector<std::string> localLogsQueue;
+            {
+                std::lock_guard<std::mutex> lock(mutex);
+                std::swap(localLogsQueue, logsQueue);
+            }
+
+            for (auto it = localLogsQueue.begin(); it != localLogsQueue.end();
+                 it++) {
+                std::cout << *it + '\n';
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+}
+
+void InitializeLogThread() {
+    if (isLogThread) {
+        return;
+    }
+    logThread = std::thread(LogThreadCycle);
+    logThread.detach();
+}
+// TODO: deinitialize
+
 void Info(std::string input) {
+    if (!isLogThread) {
+        InitializeLogThread();
+    }
     if (globalLevel == LogLevel::Info || globalLevel == LogLevel::Debug) {
-        std::cout << input + '\n';
+        std::lock_guard<std::mutex> lock(mutex);
+        logsQueue.push_back(input);
     }
 }
 
 void Error(std::string input) {
+    if (!isLogThread) {
+        InitializeLogThread();
+    }
     if (globalLevel == LogLevel::Error || globalLevel == LogLevel::Info ||
         globalLevel == LogLevel::Debug) {
-        std::cout << input + '\n';
+        std::lock_guard<std::mutex> lock(mutex);
+        logsQueue.push_back(input);
     }
 }
 
 void Debug(std::string input) {
+    if (!isLogThread) {
+        InitializeLogThread();
+    }
     if (globalLevel == LogLevel::Debug) {
-        std::cout << input + '\n';
+        std::lock_guard<std::mutex> lock(mutex);
+        logsQueue.push_back(input);
     }
 }
