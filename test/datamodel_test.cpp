59eaf40d02add22f9706688f301bf041086e235b
abramovgeorge <86909516+abramovgeorge@users.noreply.github.com>
2021-07-12 16:09:21 +0700

    Datamodel: create table from journal (#43)
    * table from journal
    
    * fixed journal_ and GetValue + added StringToOps
    
    * added StringToOp + fixes
    
    * removed keys in tables
    
    * private fix

--- test/datamodel_test.cpp
+++ test/datamodel_test.cpp
@@ -6,8 +6,9 @@
 TEST_CASE("Datamodel(set get)", "[set get]") {
     DbSettings settings;
     settings.in_memory = true;
+    settings.journal_limit = 10;
     auto storage = CreateStorage(settings);
-    auto datamodel = CreateDatamodel(storage, DbSettings{});
+    auto datamodel = CreateDatamodel(storage, settings);
 
     Operations ops;
     ops.push_back(Op{"key", "value", Op::Type::Update});
@@ -23,8 +24,9 @@ TEST_CASE("Datamodel(set get)", "[set get]") {
 TEST_CASE("Datamodel(set + remove get)", "[set get]") {
     DbSettings settings;
     settings.in_memory = true;
+    settings.journal_limit = 10;
     auto storage = CreateStorage(settings);
-    auto datamodel = CreateDatamodel(storage, DbSettings{});
+    auto datamodel = CreateDatamodel(storage, settings);
 
     Operations ops;
     ops.push_back(Op{"key", "value", Op::Type::Update});
@@ -41,8 +43,9 @@ TEST_CASE("Datamodel(set + remove get)", "[set get]") {
 TEST_CASE("Datamodel(set get w/ empty key)", "[set get]") {
     DbSettings settings;
     settings.in_memory = true;
+    settings.journal_limit = 10;
     auto storage = CreateStorage(settings);
-    auto datamodel = CreateDatamodel(storage, DbSettings{});
+    auto datamodel = CreateDatamodel(storage, settings);
 
     std::string value;
 
@@ -52,8 +55,9 @@ TEST_CASE("Datamodel(set get w/ empty key)", "[set get]") {
 TEST_CASE("Datamodel(set get w/ spaces)", "[set get]") {
     DbSettings settings;
     settings.in_memory = true;
+    settings.journal_limit = 10;
     auto storage = CreateStorage(settings);
-    auto datamodel = CreateDatamodel(storage, DbSettings{});
+    auto datamodel = CreateDatamodel(storage, settings);
 
     Operations ops;
     ops.push_back(Op{"key with spaces", "value with spaces", Op::Type::Update});
@@ -68,11 +72,61 @@ TEST_CASE("Datamodel(set get w/ spaces)", "[set get]") {
 TEST_CASE("Datamodel(set get w/ spaces and overlapping keys)", "[set get]") {
     DbSettings settings;
     settings.in_memory = true;
+    settings.journal_limit = 10;
     auto storage = CreateStorage(settings);
-    auto datamodel = CreateDatamodel(storage, DbSettings{});
+    auto datamodel = CreateDatamodel(storage, settings);
+
     Operations ops;
     ops.push_back(Op{"key space test", "value with spaces", Op::Type::Update});
     datamodel->Commit(ops);
     std::string value;
+
     REQUIRE(datamodel->GetValue("key space", value) == false);
+}
+
+TEST_CASE("Datamodel(create table from journal + get value from table)",
+          "[set get]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    settings.journal_limit = 4;
+    auto storage = CreateStorage(settings);
+    auto datamodel = CreateDatamodel(storage, settings);
+
+    Operations ops;
+    ops.push_back(Op{"key 1", "value 1", Op::Type::Update});
+    ops.push_back(Op{"key 2", "value 2", Op::Type::Update});
+    ops.push_back(Op{"key 3", "value 1 2 3", Op::Type::Update});
+    ops.push_back(Op{"key 2", "", Op::Type::Remove});
+    ops.push_back(Op{"key 1", "value", Op::Type::Update});
+
+    std::string value;
+
+    datamodel->Commit(ops);
+    datamodel->Commit({});
+    datamodel->GetValue("key 1", value);
+
+    REQUIRE(storage->GetTableList()->GetTable(0) == "");
+    REQUIRE(storage->GetTableList()->GetTable(1) ==
+            "5 key 15 value5 key 20 5 key 311 value 1 2 3");
+    REQUIRE(value == "value");
+}
+
+TEST_CASE("Datamodel(empty values in table)", "[set get]") {
+    DbSettings settings;
+    settings.in_memory = true;
+    settings.journal_limit = 1;
+    auto storage = CreateStorage(settings);
+    auto datamodel = CreateDatamodel(storage, settings);
+
+    Operations ops;
+    ops.push_back(Op{"key", "value", Op::Type::Update});
+    ops.push_back(Op{"key", "", Op::Type::Remove});
+
+    std::string value;
+
+    datamodel->Commit(ops);
+    datamodel->Commit({});
+
+    REQUIRE(storage->GetTableList()->GetTable(1) == "3 key0 ");
+    REQUIRE(datamodel->GetValue("key", value) == false);
 }
