e21cc108e9d615422e6a9d459480d8e0b9e28315
ruca905 <yarkivaev@mail.ru>
2021-07-13 20:41:19 +0700

    Make commit func miltithreaded

--- lib/src/ssyp_db.cpp
+++ lib/src/ssyp_db.cpp
@@ -1,5 +1,10 @@
 #include "../include/ssyp_db.h"
 
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <chrono>
+
 #include "datamodel.h"
 
 class Transaction : public ITransaction {
@@ -71,13 +76,50 @@ public:
         }
     }
     CommitStatus Commit(ITransactionPtr tx) override {
-        return datamodel_->Commit(((Transaction*)(tx.get()))->ops)
-                   ? CommitStatus::Success
-                   : CommitStatus::Error;
+	std::promise<CommitStatus> promise;
+	std::future<CommitStatus> future = promise.get_future();
+	if (!isLogThread){
+		InitializeCommitThread();
+	}
+        std::lock_guard<std::mutex> lock(mutex);
+        transactionPtrQueue_.push_back(std::make_pair(tx, &promise));
+        return future;
     }
 
 private:
     IDatamodelPtr datamodel_;
+    std::vector<std::pair<ITransactionPtr, std::promise<CommitStatus>*> > transactionPtrQueue_;
+    bool stopThread = false;
+    bool isCommitThread = false;
+    std::mutex mutex;
+
+    void InitializeCommitThread {
+	if (isCommitThread){
+            return;
+	}
+	commitThread = std::thread(CommitThreadCycle)
+	commitThread.detach()
+    }
+    void CommitThreadCycle() {
+        while(!stopThread) {
+            if (!transactionQueue.empty()) {
+                std::vector<std::string> localTransactionQueue;
+                {
+                    std::lock_guard<std::mutex> lock(mutex);
+                    std::swap(localTransactionQueue, transactionQueue);
+                }
+
+                for (auto it = localTransactionQueue.begin(); it != localTransactionQueue.end();
+                     it++) {
+                    ((*it)[1])->set_value = datamodel_->Commit(((Transaction*)(((*it)[0]).get()))->ops)
+                        ? CommitStatus::Success
+                        : CommitStatus::Error;
+
+                }
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
+    }
 };
 
 std::shared_ptr<ISsypDB> CreateDb(DbSettings settings) {
