46527dddcd2b919c6e06c31d55e866884c94bbaa
siw321 <podoskin.kostya@gmail.com>
2021-07-15 16:11:51 +0700

    storage statistic

--- lib/src/in_memory_storage.cpp
+++ lib/src/in_memory_storage.cpp
@@ -2,13 +2,17 @@
 
 #include <algorithm>
 
+#include "logging.h"
+
 InMemoryTableList::InMemoryTableList(
-    std::vector<std::shared_ptr<std::string>> tables)
-    : tables_(tables) {}
+    std::vector<std::shared_ptr<std::string>> tables,
+    std::shared_ptr<int> read_table_count)
+    : tables_(tables), read_table_count_(read_table_count) {}
 
 size_t InMemoryTableList::TableCount() const { return tables_.size(); }
 
 std::string InMemoryTableList::GetTable(size_t index) const {
+    (*read_table_count_)++;
     return *(tables_.at(index));
 }
 
@@ -16,17 +20,22 @@ bool InMemoryStorage::WriteToJournal(std::vector<std::string> ops) {
     for (auto const& it : ops) {
         journal_.push_back(it);
     }
+    write_journal_count_++;
     return true;
 }
 bool InMemoryStorage::PushJournalToTable(std::string blob) {
     table_list_.push_back(std::make_shared<std::string>(blob));
     journal_.clear();
+    push_table_count_++;
     return true;
 }
 ITableListPtr InMemoryStorage::GetTableList() {
-    return std::make_shared<InMemoryTableList>(table_list_);
+    return std::make_shared<InMemoryTableList>(table_list_, read_table_count_);
 }
-JournalBlob InMemoryStorage::GetJournal() { return journal_; }
+JournalBlob InMemoryStorage::GetJournal() {
+    read_journal_count_++;
+    return journal_;
+}
 
 bool InMemoryStorage::MergeTable(std::vector<size_t> merged_tables,
                                  std::string result_table) {
@@ -36,5 +45,14 @@ bool InMemoryStorage::MergeTable(std::vector<size_t> merged_tables,
         table_list_.erase(table_list_.begin() + it);
     }
     table_list_.push_back(std::make_shared<std::string>(result_table));
+    merge_table_count_++;
     return true;
+}
+
+InMemoryStorage::~InMemoryStorage() {
+    Debug("journal writings: " + std::to_string(write_journal_count_));
+    Debug("\njournal readings: " + std::to_string(read_journal_count_));
+    Debug("\ntable pushings: " + std::to_string(push_table_count_));
+    Debug("\ntable mergings: " + std::to_string(merge_table_count_));
+    Debug("\ntable readings: " + std::to_string(*read_table_count_));
 }
--- lib/src/in_memory_storage.h
+++ lib/src/in_memory_storage.h
@@ -3,12 +3,14 @@
 
 class InMemoryTableList : public ITableList {
 public:
-    InMemoryTableList(std::vector<std::shared_ptr<std::string>> tables);
+    InMemoryTableList(std::vector<std::shared_ptr<std::string>> tables,
+                      std::shared_ptr<int> read_table_count);
     size_t TableCount() const override;
     std::string GetTable(size_t index) const override;
 
 private:
     std::vector<std::shared_ptr<std::string>> tables_;
+    std::shared_ptr<int> read_table_count_;
 };
 
 class InMemoryStorage : public IStorage {
@@ -19,8 +21,15 @@ public:
     JournalBlob GetJournal() override;
     bool MergeTable(std::vector<size_t> merged_tables,
                     std::string result_table) override;
+    ~InMemoryStorage();
 
 private:
     std::vector<std::string> journal_;
     std::vector<std::shared_ptr<std::string>> table_list_;
+
+    int write_journal_count_ = 0;
+    int read_journal_count_ = 0;
+    int push_table_count_ = 0;
+    int merge_table_count_ = 0;
+    std::shared_ptr<int> read_table_count_ = std::make_shared<int>(0);
 };
