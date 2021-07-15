1203dc09be4fff67456032952110cf27265c72a2
AntonAdonin <68176962+AntonAdonin@users.noreply.github.com>
2021-07-15 20:25:53 +0700

    first benchmark point completed

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -1,30 +1,49 @@
 #include "ssyp_db.h"
 #include <chrono>
 #include <iostream>
+#include <ctime>
+#include <ratio>
 
 int main() {
     std::cout << "This is a benchmark template!\n";
-    clock_t start, end;
-    
     DbSettings settings;
     settings.in_memory = true;
     auto db = CreateDb(settings);
-
     auto transaction = db->StartTransaction();
     db->SetValue("key1", true, transaction);
     db->SetValue("key2", 2, transaction);
     db->SetValue("key3", 3.5, transaction);
     db->SetValue("key4", "value4", transaction);
+    bool v1;
+    int v2;
+    double v3;
+    std::string v4;
 
-    std::chrono::high_resolution_clock::time_point start =
-        std::chrono::high_resolution_clock::now();
-    for (int i = 0; i <= 1000; i++)
+    const int TESTQUANTITY = 5000;
+
+    auto start = std::chrono::high_resolution_clock::now(); // первый тест на количество коммитов за 1 секунду
+    for (int i = 1; i <= 1000; i++)
     {
         db->Commit(transaction);
     }
-    std::chrono::high_resolution_clock::time_point end =
-        std::chrono::high_resolution_clock::now();
-    double diff = end - start;
-    std::cout << "commits for " << diff << "seconds. " << 1000 / diff << " commits for 1 second.";
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / double(1000);
+
+    std::cout << "1000 commits for " << time << " seconds. " << std::endl;
+    std::cout << int(1000 / time) << "commits for 1 second" << std::endl;
+
+    auto start1 = std::chrono::high_resolution_clock::now(); // второй тест на TESTQUANTITY GetValue
+    for (size_t i = 1; i <= TESTQUANTITY; i++) {
+        db->GetValue("key1", v1);
+    }
+    auto end1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << TESTQUANTITY << " GetValue() for " << std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count() / double(1000) << " seconds";
+
+
+
+
+
     return 0;
 }   
